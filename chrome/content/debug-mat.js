94784900b0e1747db93fbc6dfd9c2a6fab5f1e7f
Matthieu Codron <matthieu.codron+dev@anakeen.com>
2011-05-16 10:17:47 +0200

    add two debug windows to avoid future conflicts

---
+++ chrome/content/debug-mat.js
@@ -0,0 +1,4 @@
+// start_venkman();
+Components.utils.import("resource://modules/logger.jsm");
+Components.utils.import("resource://modules/docManager.jsm");
+// Components.utils.import("chrome://dcpoffline/content/logger.jsm");
