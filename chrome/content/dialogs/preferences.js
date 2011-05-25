8c48f2941ca13fbef0badd9be836178ce0898c89
Charles Bonnissent <charles.bonnissent@anakeen.com>
2011-05-25 17:50:51 +0200

    add a swipe preferences window

--- chrome/content/dialogs/preferences.js
+++ chrome/content/dialogs/preferences.js
@@ -2,3 +2,11 @@ const Cc = Components.classes;
 const Ci = Components.interfaces;
 const Cu = Components.utils;
 
+
+Cu.import("resource://modules/preferences.jsm");
+Cu.import("resource://modules/events.jsm");
+
+function destuctionTotal() {
+    Preferences.resetBranch("offline.user.");
+    applicationEvent.publish("close");
+}
