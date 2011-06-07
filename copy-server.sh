da72de6809726c5db25b91251e33efe7396a1140
Matthieu Codron <matthieu.codron+dev@anakeen.com>
2011-06-07 18:54:32 +0200

    prepare to distribute: remove some non distribuable packages

--- copy-server.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-pubdir=/var/www/eric/
-echo 'Components.utils.import("resource://modules/fdl-data-debug.jsm");var EXPORTED_SYMBOLS = [ "Fdl", "JSON" ];' > chrome/modules/offline-debug.jsm
-cat $pubdir/lib/offline/offline-debug.js >> chrome/modules/offline-debug.jsm
-echo  'var EXPORTED_SYMBOLS = [ "Fdl" , "JSON" ];' > chrome/modules/fdl-data-debug.jsm
-cat  $pubdir/lib/data/fdl-data-debug.js |  sed -e "s/alert(/throw(/" >> chrome/modules/fdl-data-debug.jsm
