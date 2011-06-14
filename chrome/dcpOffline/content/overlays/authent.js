918f663b27d13fdee1107831ed7b914791641bc3
Marc <marc@anakeen.com>
2011-06-14 08:59:40 +0200

    replace label by description (multi-line)

--- chrome/dcpOffline/content/overlays/authent.js
+++ chrome/dcpOffline/content/overlays/authent.js
@@ -55,9 +55,8 @@ function onAuthentSuccess() {
 
 function onAuthentError(reason) {
     logIHM("onLogError "+reason);
-
     if (reason) {
-        document.getElementById('authent.errorLabel').value = reason;
+        document.getElementById('authent.errorLabel').textContent = reason;
         document.getElementById('authent.errorGroup').style.visibility = "visible";
         document.getElementById('authent.progressGroup').hidden = true;
     }
