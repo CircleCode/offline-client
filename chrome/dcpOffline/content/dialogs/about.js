accc749cf6496245471a8724af6647e3600f3bf7
Matthieu Codron <matthieu.codron+dev@anakeen.com>
2011-09-07 17:34:48 +0200

    add an about dialog

--- chrome/dcpOffline/content/dialogs/about.js
+++ chrome/dcpOffline/content/dialogs/about.js
@@ -1,4 +1,18 @@
-const Cc = Components.classes;
-const Ci = Components.interfaces;
-const Cu = Components.utils;
+Components.utils.import("resource://gre/modules/Services.jsm");
 
+function init(aEvent) {
+    if (aEvent.target != document)
+        return;
+
+    var version = Services.appinfo.version;
+    var buildID = Services.appinfo.appBuildID;
+    document.getElementById("appVersion").value += version + " (" + buildID + ")";
+
+    var version = Services.appinfo.platformVersion;
+    var buildID = Services.appinfo.platformBuildID;
+    document.getElementById("platformVersion").value += version + " (" + buildID + ")";
+
+    var productName = Services.appinfo.name;
+    var productVendor = Services.appinfo.vendor;
+    document.getElementById("productName").value = productName + " by " + productVendor;
+}
