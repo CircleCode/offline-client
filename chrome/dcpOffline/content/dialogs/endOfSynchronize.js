cd3aef55d9eff89a92aba5c574f23b7dd395b3bb
Matthieu Codron <matthieu.codron+dev@anakeen.com>
2011-10-11 14:33:50 +0200

    add informations and links in end of synchro (closes #2082)
    since it also need some commands from main, add all commands in an overlay

--- chrome/dcpOffline/content/dialogs/endOfSynchronize.js
+++ chrome/dcpOffline/content/dialogs/endOfSynchronize.js
@@ -6,6 +6,7 @@ function initDialog() {
     var result = window.arguments[0];
 
     logConsole('endSync', result);
+
     if (result.description.status) {
         document.getElementById("resultLabel").value = result.description.status;
     } else {
@@ -15,11 +16,14 @@ function initDialog() {
             document.getElementById("resultLabel").value = getText("synchronize.fail");
         }
     }
+
     logConsole("Status", result.description);
+
     if (result.description.manageWaitingUrl) {
-        document.getElementById("report").hidden = false;
-       // document.getElementById("report").href = result.description.manageWaitingUrl;
+        document.getElementById("onlineSpaceLink").href = result.description.manageWaitingUrl;
+        document.getElementById("onlineSpaceLink").tooltipText = result.description.manageWaitingUrl;
     }
+
     if (result.description.message) {
         document.getElementById("message").hidden = false;
         document.getElementById("message").value = result.description.message;
@@ -31,12 +35,13 @@ function openServerStatusPage() {
     if (result && result.description && result.description.manageWaitingUrl) {
         // first construct an nsIURI object using the ioservice
         var ioservice = Components.classes["@mozilla.org/network/io-service;1"]
-        .getService(Components.interfaces.nsIIOService);
+                .getService(Components.interfaces.nsIIOService);
 
-        var uriToOpen = ioservice.newURI(result.description.manageWaitingUrl, null, null);
+        var uriToOpen = ioservice.newURI(result.description.manageWaitingUrl,
+                null, null);
 
         var extps = Components.classes["@mozilla.org/uriloader/external-protocol-service;1"]
-        .getService(Components.interfaces.nsIExternalProtocolService);
+                .getService(Components.interfaces.nsIExternalProtocolService);
 
         // now, open it!
         extps.loadURI(uriToOpen);
--- chrome/dcpOffline/content/main.js
+++ chrome/dcpOffline/content/main.js
@@ -254,6 +254,19 @@ function initValues() {
 /* Dialog opener */
 
 /**
+ * Open the close dialog Private method : you should never use it
+ * 
+ * @private
+ */
+function openCloseDialog() {
+    /*
+     * window.openDialog("chrome://dcpoffline/content/dialogs/close.xul", "",
+     * "chrome,modal");
+     */
+    tryToClose();
+}
+
+/**
  * Open the login dialog Private method : you should never use it
  * 
  * @private
@@ -272,51 +285,12 @@ function openNewDocumentDialog() {
             "", "chrome,modal");
 }
 /**
- * Open the close dialog Private method : you should never use it
- * 
- * @private
- */
-function openCloseDialog() {
-    /*
-     * window.openDialog("chrome://dcpoffline/content/dialogs/close.xul", "",
-     * "chrome,modal");
-     */
-    tryToClose();
-}
-/**
- * Open the preferences dialog Private method : you should never use it
- * 
- * @private
- */
-function openPreferences() {
-    window.openDialog("chrome://dcpoffline/content/dialogs/preferences.xul",
-            "", "chrome,titlebar,toolbar,centerscreen,modal");
-}
-/**
- * Open the synchro dialog Private method : you should never use it
- * 
- * @private
- */
-function openSynchro() {
-    window.openDialog("chrome://dcpoffline/content/dialogs/synchro.xul", "",
-    "chrome,modal");
-}
-/**
  * Open the log dialog Private method : you should never use it
  * 
  * @private
  */
 function openLog() {
     window.openDialog("chrome://dcpoffline/content/dialogs/log.xul", "",
-    "chrome,modal");
-}
-/**
- * Open the about dialog Private method : you should never use it
- * 
- * @private
- */
-function openAbout() {
-    window.openDialog("chrome://dcpoffline/content/dialogs/about.xul", "",
     "chrome,modal");
 }
 
@@ -974,17 +948,6 @@ function prepareDoc(param) {
 
     return true;
 }
-// shortcut
-/**
- * Get the current selected domain
- * 
- * Private method : you should never use it
- * 
- * @private
- */
-function getCurrentDomain() {
-    return Preferences.get("offline.user.currentSelectedDomain", "");
-}
 /**
  * Get the current open document
  * 
@@ -1038,47 +1001,3 @@ function reloadCreatableFamilies() {
 function logIHM(message, object) {
     logConsole(message, object);
 }
-
-function checkForUpdate(){
-    var um = 
-        Components.classes["@mozilla.org/updates/update-service;1"].
-        getService(Components.interfaces.nsIUpdateManager);
-    var prompter = 
-        Components.classes["@mozilla.org/updates/update-prompt;1"].
-        createInstance(Components.interfaces.nsIUpdatePrompt);
-    
-    if(um.activeUpdate && um.activeUpdate.state == "pending"){
-        prompter.showUpdateDownloaded(um.activeUpdate);
-    } else {
-        prompter.checkForUpdates();
-    };
-};
-
-function openSynchroReport(domainName){
-    if(!domainName){
-        var domainId = getCurrentDomain();
-        var r=storageManager
-                .execQuery({
-                    query : "select * from domains where id=:domainid",
-                        params:{
-                            domainid:domainId
-                        }
-                });
-        if (r.length == 1) {
-            domainName = r[0].name;
-        } else {
-            logIHM("openSynchroReport : could not get domain name (domain id is "+domainId+')');
-        };
-    };
-    var reportFile = Components.classes["@mozilla.org/file/directory_service;1"]
-            .getService(Components.interfaces.nsIProperties)
-            .get("ProfD", Components.interfaces.nsILocalFile);
-    reportFile.append('Logs');
-    reportFile.append('report-' + domainName + '.html');
-
-    if(reportFile.exists()){
-        reportFile.launch();
-    } else {
-        logIHM("openSynchroReport : report file does not exists: " + reportFile.path);
-    };
-};
